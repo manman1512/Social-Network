8a8821ac8ba0efdee87ba5b5cd93f547d65a3e65
ManMan <mandieutan@gmail.com>
2022-08-14 20:37:22 +0700

    update front

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -1,47 +1,68 @@
-import React from 'react'
-import '../auth.css'
-import {Input} from '../Register'
-import { useForm, FormProvider } from 'react-hook-form';
+import React from 'react';
+import '../auth.css';
+import { Input } from '../Register';
+// import { useForm, FormProvider } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import axiosClient from '../../axiosClient';
 
 export default function Login() {
-    
-  const methods = useForm();
-  const onSubmit = (data) => console.log(data);
+  // const methods = useForm();
+  // const onSubmit = (data) => console.log(data);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const username = e.target.username.value;
+    const password = e.target.password.value;
+
+    console.log(username, password);
+
+    try {
+      const response = await axiosClient.post('/auth/login', {
+        username,
+        password,
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div
       className="container-register p-5 w-full min-h-screen flex flex-wrap 
     justify-center items-center"
     >
-      <FormProvider {...methods}>
-        <div className="border-2 bg-white rounded-xl pr-14 pl-14 pb-14 pt-10">
-          <form onSubmit={methods.handleSubmit(onSubmit)}>
-            <h1 className="text-center pb-10 font-semibold text-3xl">
-              Login
-            </h1>
-            <div className="">
-              <Input placeholder="Username" id="username" name="username" />
-              <Input
-                placeholder="Password"
-                id="password"
-                name="password"
-                className="inp"
-              />
-              
-              <button 
-                className="border-2 wrap-input relative w-full h-12 outline-none py-0 px-6 mt-5 
-                bg-slate-800 text-white hover:bg-slate-600">
-                REGISTER
-              </button>
-              <div className="pt-11 font-mono">
-                <span>Already have an account?</span>
-                <Link to="/register" className="hover:text-blue-500"> Register </Link>
-              </div>
+      {/* <FormProvider {...methods}> */}
+      <div className="border-2 bg-white rounded-xl pr-14 pl-14 pb-14 pt-10">
+        <form onSubmit={handleSubmit} action="">
+          <h1 className="text-center pb-10 font-semibold text-3xl">Login</h1>
+          <div className="">
+            <Input placeholder="Username" id="username" name="username" />
+            <Input
+              placeholder="Password"
+              id="password"
+              name="password"
+              className="inp"
+            />
+
+            <button
+              className="border-2 wrap-input relative w-full h-12 outline-none py-0 px-6 mt-5 
+                bg-slate-800 text-white hover:bg-slate-600"
+              type="submit"
+            >
+              LOGIN
+            </button>
+            <div className="pt-11 font-mono">
+              <span>Already have an account?</span>
+              <Link to="/register" className="hover:text-blue-500">
+                {' '}
+                Register{' '}
+              </Link>
             </div>
-          </form>
-        </div>
-      </FormProvider>
+          </div>
+        </form>
+      </div>
+      {/* </FormProvider> */}
     </div>
   );
 }
--- client/src/components/Register/index.jsx
+++ client/src/components/Register/index.jsx
@@ -1,8 +1,8 @@
-import React, {useRef} from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import '../auth.css';
-import { useForm, FormProvider } from 'react-hook-form';
-// import axiosClient from "../../axiosClient"
+// import { useForm, FormProvider } from 'react-hook-form';
+import axiosClient from '../../axiosClient';
 // import { yupResolver } from "@hookform/resolvers/yup";
 // import * as yup from "yup";
 
@@ -15,53 +15,80 @@ export function Input(props) {
       id={id}
       placeholder={placeholder}
       className="wrap-input relative w-full h-12 outline-none py-0 px-6"
-      type={name==='password' || name === "passwordConfirm" ? "password" : "text"} 
+      type={
+        name === 'password' || name === 'passwordConfirm' ? 'password' : 'text'
+      }
     />
   );
 }
 
 export default function Register() {
-  const methods = useForm();
-  const onSubmit = (data) => console.log(data);
+  // const methods = useForm();
+  // const onSubmit = (data) => console.log(data);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const username = e.target.username.value;
+    const password = e.target.password.value;
+    const passwordConfirm = e.target.passwordConfirm.value;
+
+    console.log(username, password, passwordConfirm)
+
+    try {
+      const response = await axiosClient.post('/auth/register',{
+        username,
+        password,
+        passwordConfirm
+      })
+
+      console.log(response)
+
+    } catch (error) {
+      console.log(error)
+    }
+  };
 
   return (
     <div
       className="container-register p-5 w-full min-h-screen flex flex-wrap 
     justify-center items-center"
     >
-      <FormProvider {...methods}>
-        <div className="border-2 bg-white rounded-xl pr-14 pl-14 pb-14 pt-10">
-          <form onSubmit={methods.handleSubmit(onSubmit)}>
-            <h1 className="text-center pb-10 font-semibold text-3xl">
-              Register
-            </h1>
-            <div className="">
-              <Input placeholder="Username" id="username" name="username" />
-              <Input
-                placeholder="Password"
-                id="password"
-                name="password"
-                className="inp"
-              />
-              <Input
-                placeholder="Password Confirm"
-                id="passwordConfirm"
-                name="passwordConfirm"
-                className="inp"
-              />
-              <button 
-                className="border-2 wrap-input relative w-full h-12 outline-none py-0 px-6 mt-5 
-                bg-slate-800 text-white hover:bg-slate-600">
-                REGISTER
-              </button>
-              <div className="pt-11 font-mono">
-                <span>Already have an account?</span>
-                <Link to="/login" className="hover:text-blue-500"> Login </Link>
-              </div>
+      {/* <FormProvider {...methods}> */}
+      <div className="border-2 bg-white rounded-xl pr-14 pl-14 pb-14 pt-10">
+        <form onSubmit={handleSubmit} >
+          <h1 className="text-center pb-10 font-semibold text-3xl">Register</h1>
+          <div className="">
+            <Input placeholder="Username" id="username" name="username" />
+            <Input
+              placeholder="Password"
+              id="password"
+              name="password"
+              className="inp"
+            />
+            <Input
+              placeholder="Password Confirm"
+              id="passwordConfirm"
+              name="passwordConfirm"
+              className="inp"
+            />
+            <button
+              className="border-2 wrap-input relative w-full h-12 outline-none py-0 px-6 mt-5 
+                bg-slate-800 text-white hover:bg-slate-600"
+              type="submit"
+            >
+              REGISTER
+            </button>
+            <div className="pt-11 font-mono">
+              <span>Already have an account?</span>
+              <Link to="/login" className="hover:text-blue-500">
+                {' '}
+                Login{' '}
+              </Link>
             </div>
-          </form>
-        </div>
-      </FormProvider>
+          </div>
+        </form>
+      </div>
+      {/* </FormProvider> */}
     </div>
   );
 }
